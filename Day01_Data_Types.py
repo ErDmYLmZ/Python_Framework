d8e7d6438124d231d7d1b12d78280e09529da8b1
ErDmYLmZ <erdemyilmaz4@gmail.com>
2021-12-01 20:43:59 +0100

    01_12

---
+++ Day01_Data_Types.py
@@ -0,0 +1,37 @@
+# print("Allah muvaffak eylesin")
+# x=500
+# z="Allah muvaffak eylesin Insallah."
+# print(x*100)
+# y="Amin"
+# print(z,y)
+#
+'''
+x=800
+y="Merhaba"
+print(x,y, sep="yyy")
+print("my name is"+y)
+name=input("isminiz nedir?")
+print("sizin isminiz",name)
+girdi=input("isminizi giriniz")
+print("Merhaba "+girdi)
+'''
+if 5>2:
+    print("sonuc dogru")
+
+fruits=["orange","apple","banana"]
+x,y,z=fruits
+print (x)
+print (y)
+print (z)
+
+isim="Asen"
+soyisim="toshev"
+sehir="Ankara"
+print("isim",isim,"\n","soyisim",soyisim,"\n","Sehir", sehir)
+
+x,y,z,q="O","L","E","Y"
+print(x,y,z,q)
+
+x=input("lutfen bir sayi giriniz")
+y=input(("lutfen ikinci sayiyi giriniz"))
+print(int(x)+int(y))
---
+++ Day03_Data_Types.py
@@ -0,0 +1,19 @@
+b = "Day03"
+print(b)
+print(b[1])
+print(b[-4])
+print(len(b))
+x = 5
+a = 5
+print(id(x), id(a), sep=" <==> ")
+myBool = False
+# input_1 = input("please enter a number")
+# input_2 = input("please enter a number")
+# print(int(input_1)+int(input_2))
+
+print(len(6 * "hi"))
+myFruit = "banana"
+print(myFruit[0:7:2])
+
+s1=slice(1,4)
+print(myFruit[s1])
